93da244c6ff9a544c47ab5cb9799a2bf7e5e822e
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-04-11 21:10:10 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+
+let tarifLightBefore,
+    tarifLightAfter,
+    tarifGas;
+
+let card = document.querySelector('#card');
+
+let inputLightBefore = document.querySelector('#item-input-before');
+let inputLightAfter = document.querySelector('#item-input-after');
+
+let inputGas = document.querySelector('#item-input-gas');
+let inputGasBegin = document.querySelector('#input-gas-begin');
+let inputGasEnd = document.querySelector('#input-gas-end');
+let inputGasAbonent = document.querySelector('#input-gas-abonent');
+
+let inputWater = document.querySelector('#item-input-water');
+let inputWaterBegin = document.querySelector('#input-water-begin');
+let inputWaterEnd = document.querySelector('#input-water-end');
+let sumGasAbonent = document.querySelector('#sum-gas-abonent');
+
+
+let sumGas = document.querySelector('#sum-gas');
+let sumWater = document.querySelector('#sum-water');
+
+
+
+card.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    let tarifLightBefore = inputLightBefore.value;    
+    let tarifLightAfter = inputLightAfter.value;    
+    let tarifGas = inputGas.value;
+    let tarifWater = inputWater.value;
+
+
+    let gasBegin = inputGasBegin.value;
+    let gasEnd = inputGasEnd.value;
+    let gasAbonent = +inputGasAbonent.value;
+
+    let waterBegin = inputWaterBegin.value;
+    let waterEnd = inputWaterEnd.value;
+
+    let gas = (gasEnd - gasBegin) ;
+    let water =   waterEnd - waterBegin;
+
+    console.log(tarifLightBefore);
+    console.log(tarifLightAfter);
+    console.log(tarifGas);
+/*    console.log(gasAbonent);
+    console.log(gasBegin);*/
+    console.log(water); 
+
+
+    
+    console.log(sumGas);
+    sumGas.innerHTML =  (gasEnd - gasBegin) * tarifGas;
+    sumWater.innerHTML = water * tarifWater;
+
+   if (gasAbonent > 0) {
+    sumGasAbonent.innerHTML = ((gas * tarifGas) +gasAbonent);
+   }
+
+    
+ 
+   
+});
+
+
+     
+
+
+
+    
+
+
+
+/*    let formData ={};
+    const ls = localStorage;
+
+    function updateSearch(event) {
+        setSearch(event.target.value);  
+        formData[event.target.name] = event.target.value;
+        ls.setItem('formData', JSON.stringify(formData));
+      }
+    
+      if(ls.getItem('formData')) {
+        formData = JSON.parse(ls.getItem('formData'));
+        console.log(formData.search);
+        
+     //   setSearch(formData.search);
+      }
+
+      */
